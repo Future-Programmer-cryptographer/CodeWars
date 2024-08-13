237af7df7a05ff8b3ec66db5885a01f9115267bc
Shub <18.shubhangee.das@gmail.com>
2024-08-13 15:38:42 +0100

    did two sum problem

--- arrDiff.py
+++ arrDiff.py
@@ -11,3 +11,11 @@ def array_diff(a,b):
     return result 
 
 print(array_diff([1,2,2,2,3],[2]))
+
+""""
+We create an empty list result that will store the elements of a that are not present in b.
+We iterate over each element i in list a.
+For each element i, we check if it is not present in list b using the not in operator.
+If i is not present in b, we append it to the result list.
+Finally, we return the result list.
+"""
---
+++ twoSum.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/python
+def two_sum(numbers, target):
+    # iterate over the array to check pairs of numbers 
+    for i, val1 in enumerate(numbers):
+        for j, val2 in enumerate(numbers):
+            if i !=j and val1 + val2 == target: 
+                return i, j
+
+print(two_sum([1, 2, 3], 4))
